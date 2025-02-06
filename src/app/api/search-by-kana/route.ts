eb91d1a3c40605aa54bca9c9d6dc99e3d30a6687
dastasoft <dastasoft@protonmail.com>
2025-02-06 16:00:46 +0900

    fix: database bug 5

--- src/app/api/search-by-kana/route.ts
+++ src/app/api/search-by-kana/route.ts
@@ -1,4 +1,5 @@
 import { type NextRequest, NextResponse } from 'next/server'
+import path from 'path'
 import { open } from 'sqlite'
 import sqlite3 from 'sqlite3'
 
@@ -10,15 +11,16 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({ error: 'Invalid kana input' }, { status: 400 })
   }
 
+  const query = `SELECT * FROM kana WHERE LENGTH(value) = 7 AND value LIKE '%${kana}%'`
+
   try {
+    const dbPath = path.join(process.cwd(), 'public', 'JMdict_e.db')
     const db = await open({
-      filename: 'public/JMdict_e.db',
+      filename: dbPath,
       driver: sqlite3.Database,
     })
 
-    const items = await db.all(
-      `SELECT * FROM kana WHERE LENGTH(value) = 7 AND value LIKE '%${kana}%'`,
-    )
+    const items = await db.all(query)
 
     return NextResponse.json({ data: items })
   } catch (error) {
